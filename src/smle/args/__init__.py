9de9a7b75b2fc5b446716989c932e47ea679c997
blkdmr <me@russoalessio.dev>
2025-11-19 17:23:47 +0100

    fix: fixed importing

--- src/smle/args/__init__.py
+++ src/smle/args/__init__.py
@@ -1,1 +1,8 @@
-from smle.args.args import read_configuration_file
+import yaml
+
+def read_configuration_file(config_file:str ="smle.yaml"):
+
+    with open(config_file) as f:
+        args = yaml.safe_load(f)
+
+    return args
--- src/smle/args/args.py
+++
@@ -1,8 +0,0 @@
-import yaml
-
-def read_configuration_file(config_file:str ="smle.yaml"):
-
-    with open(config_file) as f:
-        args = yaml.safe_load(f)
-
-    return args
